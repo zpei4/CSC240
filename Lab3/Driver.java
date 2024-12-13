996bdabd60f76e4145486dd60b40ceacb6199200
zpei4 <95053032+zpei4@users.noreply.github.com>
2024-12-12 23:38:21 -0600

    Update and rename Policy.java to Driver.java

---
+++ Lab3/Driver.java
@@ -0,0 +1,23 @@
+public class Driver {
+
+ 
+
+       public static void main(String[] args)
+
+       {
+
+             // Create commission calculator
+
+             CommissionCalculator calc = new CommissionCalculator();
+
+            
+
+             // Run commission calculator
+
+             calc.Run();
+
+       }
+
+ 
+
+}
